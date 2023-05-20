a4f46bcd228fde182740cf340b18257431f40bc4
samuele4414 <samuele.despo@gmail.com>
2023-05-20 13:45:33 +0200

    tableView working

--- src/main/java/com/example/personal/BasicClass/Exercise.java
+++ src/main/java/com/example/personal/BasicClass/Exercise.java
@@ -1,88 +1,56 @@
 package com.example.personal.BasicClass;
 
 import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Exercise {
-    public final String name;
-    public final String muscleGroup;
-/*
-    public final IntegerProperty series;
 
-    public final IntegerProperty repetitions;
+    public  String name;
+    public  String muscleGroup;
+    public  Integer series;
+    public  Integer repetitions;
+    public  Double weight;
+    public Exercise(){
 
-    public final DoubleProperty weight;
-
+    }
     public Exercise(String name, String muscleGroup, Integer series, Integer repetitions, Double weight) {
-        this.name = new SimpleStringProperty(name);
-        this.muscleGroup = new SimpleStringProperty(muscleGroup);
-        this.series = new SimpleIntegerProperty(series);
-        this.repetitions = new SimpleIntegerProperty(repetitions);
-        this.weight = new SimpleDoubleProperty(weight);
-    }
-
-    public String getName() {
-        return name.get();
-    }
-
-    public StringProperty nameProperty() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    public String getMuscleGroup() {
-        return muscleGroup.get();
-    }
-
-    public StringProperty muscleGroupProperty() {
-        return muscleGroup;
-    }
-
-    public void setMuscleGroup(String muscleGroup) {
-        this.muscleGroup.set(muscleGroup);
-    }
-
-    public int getSeries() {
-        return series.get();
-    }
-
-    public IntegerProperty seriesProperty() {
-        return series;
-    }
-
-    public void setSeries(int series) {
-        this.series.set(series);
-    }
-
-    public int getRepetitions() {
-        return repetitions.get();
-    }
-
-    public IntegerProperty repetitionsProperty() {
-        return repetitions;
-    }
-
-    public void setRepetitions(int repetitions) {
-        this.repetitions.set(repetitions);
-    }
-
-    public double getWeight() {
-        return weight.get();
-    }
-
-    public DoubleProperty weightProperty() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight.set(weight);
-    }*/
-
-    public Exercise(String name, String muscleGroup) {
         this.name = name;
         this.muscleGroup = muscleGroup;
+        this.series = series;
+        this.repetitions = repetitions;
+        this.weight = weight;
     }
+    public String getName() {
 
+        return name;
+    }
+    public void setName(String name) {
+
+        this.name = name;
+    }
+    public String getMuscleGroup() {
+            return muscleGroup;
+        }
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+    public int getSeries() {
+        return series;
+    }
+    public void setSeries(int series) {
+        this.series=series;
+    }
+    public int getRepetitions() {
+        return repetitions;
+    }
+    public void setRepetitions(int repetitions) {
+        this.repetitions=repetitions;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight=weight;
+    }
 }
--- src/main/java/com/example/personal/GymController.java
+++ src/main/java/com/example/personal/GymController.java
@@ -5,6 +5,7 @@ import com.example.personal.BasicClass.Exercise;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -18,82 +19,33 @@ public class GymController {
     private TableView<Exercise> Extable;
     @FXML
     private TableColumn<Exercise, String> exerciseColumn;
-
     @FXML
     private TableColumn<Exercise, String> muscleGroupColumn;
-
-    @FXML
-    private TableColumn<Exercise, Integer> repetitionColumns;
-
     @FXML
     private TableColumn<Exercise, Integer> seriesColumn;
-
     @FXML
-    private TableColumn<Exercise, Double> weightColumn;
-
-    ObservableList<Exercise> list = FXCollections.observableArrayList(
-        new Exercise("Panca pianca", "Petto"),
-            new Exercise("Panca pianca", "Petto")
-
-);
-
+    private TableColumn<Exercise, Integer> repetitionColumns;
+    @FXML
+   private TableColumn<Exercise, Double> weightColumn;
+    /**
+     * Initializes the controller class. This method is automatically called after the fxml file has been loaded.
+     */
+    @FXML
     public void initialize() {
         // Initialize the person table with the two columns.
-
-        exerciseColumn.setCellValueFactory(new PropertyValueFactory<Exercise, String>("eser"));
-        muscleGroupColumn.setCellValueFactory(new PropertyValueFactory<Exercise, String>("Gruppo muscolare"));
-
-
-        //repetitionColumns.setCellValueFactory(new PropertyValueFactory<>("Serie"));
-        //seriesColumn.setCellValueFactory(new PropertyValueFactory<>("Ripetizioni"));
-        //weightColumn.setCellValueFactory(new PropertyValueFactory<>("Peso"));
-        Extable.setItems(list);
-        // Crea una lista osservabile di esercizi
-        //ObservableList<Exercise> exercises = FXCollections.observableArrayList();
-
-        // Aggiungi esercizi alla lista
-        //exercises.add(new Exercise("Push-up", "kk", 10, 2, 4.5));
-
-        // Imposta la lista di esercizi come dati della TableView
-
-
-        //Extable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue)->showExerciseDetails(newValue));
-
-        //Extable.setItems(getExerciseData());
-        //Extable.getItems().add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-
-
-        // Clear person details.
-        //showExerciseDetails(null);
-        // Listen for selection changes and show the person details when changed.
+        exerciseColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        muscleGroupColumn.setCellValueFactory(new PropertyValueFactory<>("muscleGroup"));
+        seriesColumn.setCellValueFactory(new PropertyValueFactory<>("series"));
+        repetitionColumns.setCellValueFactory(new PropertyValueFactory<>("repetitions"));
+        weightColumn.setCellValueFactory(new PropertyValueFactory<>("weight"));
+        Extable.setItems(getExerciseData());
     }
-/*
-    @FXML
-    public void DioKarim () {
-        Extable.getItems().add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-    }
+
     ObservableList<Exercise> getExerciseData() {
         ObservableList<Exercise> exercises = FXCollections.observableArrayList();
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-        exercises.add(new Exercise("Panca pianca", "Petto", 2, 10, 100.2 ));
-
+        exercises.add(new Exercise("sega", "pene", 1,10,5.0));
+        exercises.add(new Exercise("dio", "karim", 1,10,5.0));
         return exercises;
     }
-
- */
-
-   /* int selectedIndex() {
-        int selectedIndex = Extable.getSelectionModel().getSelectedIndex();
-        if (selectedIndex < 0) {
-            throw new NoSuchElementException();
-        }
-        return selectedIndex;
-    }*/
 }
+
